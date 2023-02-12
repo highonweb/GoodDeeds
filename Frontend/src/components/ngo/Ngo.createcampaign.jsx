f00a5ea10765f43b1c5136f57694d8cdecf1023e
Rajesh000730 <rajeshrachit09@gmail.com>
2023-02-12 05:30:44 +0530

    added auth

--- Frontend/src/components/ngo/Ngo.createcampaign.jsx
+++ Frontend/src/components/ngo/Ngo.createcampaign.jsx
@@ -4,6 +4,12 @@ import './ngo.createcampaign.css'
 import { Button, Input, Heading, Textarea } from '@chakra-ui/react'
 import { Navigate } from 'react-router-dom'
 function NgoCreateCampaign() {
+    const [auth,setAuth] = useState(false)
+    React.useEffect(()=>{
+      if(localStorage.getItem('jwt')){
+        setAuth(true)
+     }
+    }) 
     const [title, setTitle] = React.useState('')
     const [image, setImageUrl] = React.useState('')
     const [description, setDescription] = React.useState('')
@@ -30,6 +36,9 @@ function NgoCreateCampaign() {
           <Navigate to='/home' replace  />
         )
         console.log(res.data)
+    }
+    if(!auth){
+      return <Navigate to='signin' replace/>
     }
   return (
     <div id='createcampaign'>
